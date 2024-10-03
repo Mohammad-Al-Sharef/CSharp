0a909e5daa8da1cbb9f6d7916173797bf6dd87b4
Mohammad-Al-Sharef <www.malsharef57@gmail.com>
2024-10-03 14:50:15 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System.Xml.Linq;
+//Q1
+
+//Console.WriteLine("Plaese Enter Student Name");
+//string Student_Name = Console.ReadLine();
+//Console.WriteLine("Student Name is: " + Student_Name);
+
+
+//Q2
+//string name = "Mohammad";
+//int age = 24;
+//double Salary = 500;
+//char ch_Name= 'M';
+//bool working= false;
+//const string job = "governor";
+//Console.WriteLine($"Name: {name}, Age: {age}, Salary: {Salary}, First letter of the name: {ch_Name}, Working: {working}, Job: {job}");
+
+//Q3
+//string[] cars = { "Kia", "BMW", "volvo" };
+//Console.WriteLine("Length array : "+cars.Length);
+//foreach (string i in cars) {
+
+//    Console.WriteLine("Names cars : "+i);
+//}
+
+//Q4
+//Console.WriteLine("Plese Enter first name");
+//string firstname=Console.ReadLine();
+//Console.WriteLine("Plese Enter Last Name");
+//string lastname = Console.ReadLine();
+//Console.WriteLine("Plese Enter year of birth");
+//string birth = Console.ReadLine();
+
+//Console.WriteLine($"first name : { firstname} ,Last name : {lastname},year of birth : {birth}");
+
+//Q5
+
+//int[] number = new int[10];
+//Console.WriteLine(" Please Enter 10 number on array : ");
+
+//for (int i = 0; i < number.Length ; i++)
+//{
+//    Console.Write($" Enter number {i + 1}: ");
+//    number[i] = Convert.ToInt32(Console.ReadLine());  
+//}
+
+//foreach (int i in number) {
+//    Console.WriteLine(" Elements in array are " +i);
+//}
+
+//Q6 
+//int[] number = {2,5,8 };
+//int sum=0;
+
+//for ( int i = 0; i < number.Length; i++) {
+//    sum += number[i];
+//}
+//Console.WriteLine("Sum = "+sum);
